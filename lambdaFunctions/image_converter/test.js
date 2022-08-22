4ab039af4741b4d7ebdae4d4e4d5285d9911c7fe
Abhilash Shettigar <abhilashshettigar@gmail.com>
2022-08-22 22:11:28 +0530

    Update test.js

--- lambdaFunctions/image_converter/test.js
+++ lambdaFunctions/image_converter/test.js
@@ -2,6 +2,3 @@ require('dotenv').config();
 console.log('its also works:-'+process.env.secert)
 console.log('its works:-'+process.env.secert)
 console.log('its -'+process.env.secert)
-// function name(params) {
-    
-// }
